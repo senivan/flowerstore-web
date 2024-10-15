c52de5454126305242c3c441b746d346dd3c29fa
Ivan Sen <senivan1997@gmail.com>
2024-10-16 01:47:42 +0300

    Added interface for delivery strategy

--- src/main/java/com/example/demo/Order.java
+++ src/main/java/com/example/demo/Order.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import java.util.List;
 
 import com.example.demo.payment.PaymentStrategy;
-
+import com.example.demo.delivery.DeliveryStrategy;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +15,7 @@ public class Order {
     private double totalPrice;
     private static int orderCount = 0;
     private boolean is_paid = false;
+    private DeliveryStrategy wayOfDelivery;
     public Order(List<Item> items){
         this.items = items;
         this.orderId = orderCount++;
@@ -34,6 +35,14 @@ public class Order {
     public void pay(PaymentStrategy paymentStrategy){
         if (paymentStrategy.pay(totalPrice)){
             is_paid = true;
+        }
+    }
+    public void setDeliveryStrategy(DeliveryStrategy wayOfDelivery){
+        this.wayOfDelivery = wayOfDelivery;
+    }
+    public void deliver(){
+        if (is_paid){
+            wayOfDelivery.deliver();
         }
     }
 }
---
+++ src/main/java/com/example/demo/delivery/DeliveryStrategy.java
@@ -0,0 +1,5 @@
+package com.example.demo.delivery;
+
+public interface DeliveryStrategy {
+    public boolean deliver();
+}
